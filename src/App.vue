db370f0fe0c9164493ae33048490dfd3f856543f
Farah Audina <farahaudina06102006@gmail.com>
2024-05-07 21:14:39 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,10 @@ function increment() {
 </script>
 
 <template>
-  <img src="/logo.png" alt="">
+  <img src="/download.jpeg" alt="">
   <div>
-    <p v-bind:class='titleclass'> Cafe biasanya di desain dengan sangat menarik dan instagram-able supaya membuat tamu merasa nyaman dan betah berlama-lama di cafe.
-Untuk menambah daya tarik, tidak jarang mereka membuat sebuah hiburan dengan menghadirkan sebuah live music untuk hari-hari libur atau weekend, yang bertujuan untuk menemani pengunjung bersantai dan berbincang dengan alunan musik yang di suguhkan.
-
-Secara Harfiah cafe adalah minuman (kopi), dan berubah menjadi sebuah tempat yang menyediakan berbagai macam minuman dan bukan hanya kopi, terkadang minuman dengan kadar alkohol rendah ada dalam daftar menu.</p>
-<p v-bind:id='dynamicId'> Sekarang Silahkan isi menu anda pada kolom dibawah</p><br>
-<p class="text">Masukkan Menu: {{ Pesan }}</p>
+    <p v-bind:class='titleclass'> PT Toys Games Indonesia (Toys Kingdom) yang didirikan tahun 2010 menyediakan lebih dari 23.000 koleksi lengkap mainan dan hobi untuk segala usia, yang terbagi menjadi enam kategori, yaitu babies, girls, boys, education, family fun, dan outdoor</p><br>
+<p class="text">Nama Mainan: {{ Pesan }}</p>
 <input v-model="Pesan" placeholder="Isi disini" />
   </div>
   <br>
@@ -27,21 +23,17 @@ Secara Harfiah cafe adalah minuman (kopi), dan berubah menjadi sebuah tempat yan
 </template>
 
 <style>
-div {
-  background-image: url('/img.jpg');
-  background-repeat: no-repeat;
-  background-size: cover;
-}
+
 img {
  background-position: center; 
 }
 .title{
-  color: black;
+  color: rgb(255, 255, 255);
 }
 .text {
-  color: black;
+  color: rgb(255, 255, 255);
 }
 #title {
-  color: rgb(0, 0, 0);
+  color: rgb(255, 255, 255);
 }
 </style>
